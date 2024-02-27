215b96b4849a458bdefbc336934d55de6aba2612
frustrated018 <niloyd018@gmail.com>
2024-02-28 00:42:15 +0600

    Updates
    Added a font called Kode Mono
    Added the prettier plugin for tailwind classes
    Updated the layout
    updated the title and description for the metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <div className="h-[50rem] w-full bg-background bg-grid-[#27272A]/[.2] dark:bg-grid-[#F4F4F5]/[.2] relative flex items-center justify-center">
+    <div className="relative flex h-[50rem] w-full items-center justify-center bg-background bg-grid-[#27272A]/[.2] dark:bg-grid-[#F4F4F5]/[.2]">
       {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text py-8">
-        Grid Background
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <p className="relative z-20 bg-clip-text py-8 text-4xl font-bold sm:text-7xl">
+        Kode Mono Font
       </p>
     </div>
   );
---
+++ src/fonts/fonts.ts
@@ -0,0 +1,8 @@
+import localFont from "next/font/local";
+
+const Kode_Mono = localFont({
+  src: "./KodeMono-VariableFont_wght.ttf",
+  display: "swap",
+});
+
+export default Kode_Mono;
