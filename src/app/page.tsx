5190acb4209b36b425e45df5d3ab812c6aef71af
frustrated018 <niloyd018@gmail.com>
2024-02-28 22:40:50 +0600

    Added text generate effect
    Added the text generate effect forom Aceternity ui using framer motion
    Modified it to use custom duration and stagger...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { TextGenerateEffect } from "@/components/TextGenerateEffect";
+
 export default function Home() {
   return (
-    <div className="relative flex h-[50rem] w-full items-center justify-center bg-background bg-grid-[#27272A]/[.2] dark:bg-grid-[#F4F4F5]/[.2]">
+    <div className="relative h-screen w-full bg-background bg-grid-[#27272A]/[.2] dark:bg-grid-[#F4F4F5]/[.2]">
       {/* Radial gradient for the container to give a faded look */}
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <p className="relative z-20 bg-clip-text py-8 text-4xl font-bold sm:text-7xl">
-        Kode Mono Font
-      </p>
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_40%,black)]"></div>
+      {/* Hero text generate effect */}
+      <MaxWidthWrapper>
+        <div className="absolute flex flex-col gap-7">
+          <TextGenerateEffect words="Hi, my name is niloy... ANd fucko you" />
+          <TextGenerateEffect
+            stag={0.5}
+            duration={3}
+            words="This is the stagger and duration time delay for the second time..."
+          />
+        </div>
+      </MaxWidthWrapper>
     </div>
   );
 }
